88a5b53a18906666a2f468b71ba26b006c660c4c
theonlyjames <james@jamesmk.com>
2013-05-08 23:11:15 -0700

    added keys in right place

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     warn "*" * 80
     warn "WARNING: Missing consumer key or secret. First, register an app with Twitter at"
     warn "https://dev.twitter.com/apps to obtain OAuth credentials. Then, start the server"
-    warn "with the command: CONSUMER_KEY=Q2sqL3uNuWhCMZcIWByHQ CONSUMER_SECRET=m8Tbrip1k4V1o0JA4CZXyyEh9TatYMJNeMSIHoHa4n0 rails server"
+    warn "with the command: CONSUMER_KEY=abc CONSUMER_SECRET=123 rails server"
     warn "*" * 80
   else
     provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
