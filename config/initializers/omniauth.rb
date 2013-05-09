535e1c63fa4c92aa536c548ecbaace275cd1508d
theonlyjames <james@jamesmk.com>
2013-05-08 23:14:20 -0700

    another one for the win

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if ENV['CONSUMER_KEY'].blank? || ENV['CONSUMER_SECRET'].blank?
+  if ENV['Q2sqL3uNuWhCMZcIWByHQ'].blank? || ENV['m8Tbrip1k4V1o0JA4CZXyyEh9TatYMJNeMSIHoHa4n0'].blank?
     warn "*" * 80
     warn "WARNING: Missing consumer key or secret. First, register an app with Twitter at"
     warn "https://dev.twitter.com/apps to obtain OAuth credentials. Then, start the server"
     warn "with the command: CONSUMER_KEY=abc CONSUMER_SECRET=123 rails server"
     warn "*" * 80
   else
-    provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+    provider :twitter, ENV['Q2sqL3uNuWhCMZcIWByHQ'], ENV['m8Tbrip1k4V1o0JA4CZXyyEh9TatYMJNeMSIHoHa4n0']
   end
 end
