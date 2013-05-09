5b6b4394926e5ba263248b351e3b79550c4b1e36
theonlyjames <james@jamesmk.com>
2013-05-08 23:22:40 -0700

    db migrate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def client
     Twitter.configure do |config|
-      config.consumer_key = ENV['Q2sqL3uNuWhCMZcIWByHQ']
-      config.consumer_secret = ENV['m8Tbrip1k4V1o0JA4CZXyyEh9TatYMJNeMSIHoHa4n0']
-      config.oauth_token = session['625027245-J7jRwl4SkGo29dkoqjtaoVyuMiQf6eHb1Xn6pEzP']
-      config.oauth_token_secret = session['xVoPT3inx3Ua3sbXQmQ9yh62nSBOylcrMuwL7gbOO6o']
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.oauth_token = session['access_token']
+      config.oauth_token_secret = session['access_secret']
     end
   end
 
